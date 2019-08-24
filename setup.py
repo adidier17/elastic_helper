2535e96d39aee66b9cead5d9e85c6be3ec695072
Annie Didier <anniedidier2017@u.northwestern.edu>
2019-08-24 10:58:06 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -11,51 +11,13 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
     name='elastic_helper',  # Required
-
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',  # Required
+    version='0.0.1',  # Required
     scripts=['elastic_helper'],
-
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
     description='A simple wrapper class for python elasticsearch',  # Optional
-    
     url='https://github.com/adidier17/elastic_helper',  # Optional
-
-    # This should be your name or the name of the organization which owns the
-    # project.
     author='Annie Didier',  # Optional
-    
-    # Specify which Python versions you support. In contrast to the
-    # 'Programming Language' classifiers above, 'pip install' will check this
-    # and refuse to install the project if the version does not match. If you
-    # do not support Python 2, you can simplify this to '>=3.5' or similar, see
-    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
     python_requires='>=3',
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['elasticsearch==7.0.4']  # Optional
+    packages=find_packages(),
+    install_requires=['elasticsearch==7.0.4'],  # Optional
 )
